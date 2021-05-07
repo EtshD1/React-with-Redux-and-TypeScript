b1695188eb9147041939c1b99cdb06e4ace1d816
EtshD1 <H79Pain@Gmail.com>
2021-05-07 12:35:16 +0200

    add counter

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,22 @@
 import React from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { increment, decrement } from './redux/actions';
+
+import { RootState } from './redux/reducers';
 
 function App() {
+  const counter = useSelector((state: RootState) => state.counter);
+
+  const dispatch = useDispatch();
 
   return (
     <div className="App">
       <h1>Hello, world</h1>
+      <div>
+        <h3>{counter}</h3>
+        <button onClick={() => dispatch(increment(2))}>+</button>
+        <button onClick={() => dispatch(decrement(1))}>-</button>
+      </div>
     </div>
   );
 }
