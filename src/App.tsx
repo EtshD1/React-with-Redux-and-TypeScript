ba03ef1b19ec37df6bfaab3501ceb53d5d4e5a47
EtshD1 <H79Pain@Gmail.com>
2021-05-07 12:35:31 +0200

    add loggin

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import { useSelector, useDispatch } from 'react-redux';
-import { increment, decrement } from './redux/actions';
+import { increment, decrement, loggin, loggout } from './redux/actions';
 
 import { RootState } from './redux/reducers';
 
 function App() {
   const counter = useSelector((state: RootState) => state.counter);
+  const logged = useSelector((state: RootState) => state.logged);
 
   const dispatch = useDispatch();
 
@@ -16,6 +17,15 @@ function App() {
         <h3>{counter}</h3>
         <button onClick={() => dispatch(increment(2))}>+</button>
         <button onClick={() => dispatch(decrement(1))}>-</button>
+      </div>
+      <div>
+        {logged ? <div>
+          <h2>You are Logged in</h2>
+          <button onClick={() => dispatch(loggout())}>Log out</button>
+        </div> : <div>
+          <h2>Sign in</h2>
+          <button onClick={() => dispatch(loggin())}>Log in</button>
+        </div>}
       </div>
     </div>
   );
